58cbf5950d31f384629f8a76ce73137a2ae4e577
dwood7399 <woodham.dave@gmail.com>
2016-04-24 16:08:45 -0400

    Adds upcoming 3 departure times

--- js/app/views/mbta.js
+++ js/app/views/mbta.js
@@ -22,30 +22,38 @@ var MBTAView = Backbone.View.extend({
 	render: function(d) {
 		d = JSON.parse(d.body);
 		
-		var departure = d.mode[0].route[0].direction[0].trip[0].pre_away;
+		var departure1= d.mode[0].route[0].direction[0].trip[0].pre_away;
+		var departure2= d.mode[0].route[0].direction[0].trip[1].pre_away;
+		var departure3= d.mode[0].route[0].direction[0].trip[2].pre_away;
 
-		var $mbtaIcon = this.getMBTAIcon(departure);
-    		var mbtaString = this.getMBTAString(departure);
+		var $mbtaIcon = this.getMBTAIcon(departure2);
+    		var mbtaString = this.getMBTAString(departure1,departure2,departure3);
 
 		this.$el.html(mbtaString);
 		this.$el.prepend($mbtaIcon);
 	},
 
-	getMBTAString: function(departure) {
+	getMBTAString: function(d1,d2,d3) {
 		var time;
-		var minutesAway = departure / 60;
+		
+		var minutesAway = d1 / 60;
 		minutesAway =  Math.floor(minutesAway)
-
+		
+		var minutesAway2 = d2 / 60;
+		minutesAway2 =  Math.floor(minutesAway2)
+		var minutesAway3 = d3 / 60;
+		minutesAway3 =  Math.floor(minutesAway3)
+		
 		if ( minutesAway === 1 ) {
 			time = 'minute';
 		} else {
 			time = 'minutes';
 		}
 
-		var s = 'Next train  in ' + minutesAway + ' ' + time;
+		var s = 'Next train  in ' + minutesAway + ' ' + time +'. Then, ' +  minutesAway2 + ' and ' + minutesAway3 +'.';
 		return s;
 	},
-	getMBTAIcon: function(departure) {
+	getMBTAIcon: function(departure2) {
 		var src = '/assets/train.png'; //MBTA has no icons, try switching to generic icons or use manual iceon (needs new image)
 		return $('<img>').attr('src', src);
 	} 
