cb431bb8e32d81f9aed5e64cb9fe441d83fc8cb9
David Woodham <woodham.dave@gmail.com>
2016-07-31 21:07:25 -0400

    Adds 4th departure time, making next plus three upcoming, instead of 2

--- js/app/views/mbta.js
+++ js/app/views/mbta.js
@@ -25,15 +25,16 @@ var MBTAView = Backbone.View.extend({
 		var departure1= d.mode[0].route[0].direction[0].trip[0].pre_away;
 		var departure2= d.mode[0].route[0].direction[0].trip[1].pre_away;
 		var departure3= d.mode[0].route[0].direction[0].trip[2].pre_away;
+		var departure4= d.mode[0].route[0].direction[0].trip[3].pre_away;
 
 	//	var $mbtaIcon = this.getMBTAIcon(departure2);
-    		var mbtaString = this.getMBTAString(departure1,departure2,departure3);
+    		var mbtaString = this.getMBTAString(departure1,departure2,departure3,departure4);
 
 		this.$el.html(mbtaString);
 	//	this.$el.prepend($mbtaIcon);
 	},
 
-	getMBTAString: function(d1,d2,d3) {
+	getMBTAString: function(d1,d2,d3,d4) {
 		var time;
 		var time2;
 		
@@ -49,7 +50,9 @@ var MBTAView = Backbone.View.extend({
 		minutesAway2 =  Math.floor(minutesAway2)
 		var minutesAway3 = d3 / 60;
 		minutesAway3 =  Math.floor(minutesAway3)
-		
+		var minutesAway4 = d4 / 60;
+		minutesAway4 =  Math.floor(minutesAway4)
+				
 		if ( minutesAway === 1 ) {
 			time = 'min';
 		} else if (minutesAway === 'Now') {
@@ -62,8 +65,7 @@ var MBTAView = Backbone.View.extend({
 		} else {
 			time2 = 'mins';
 		}
-		var icon =  '<img src="/assets/blue-line-icon.png">'		
-		var s = '<div class="nextTrain">' + icon + minutesAway + ' <span class="thenTrains"> ' + time + '</span></div><div class="thenTrains">' +  minutesAway2 + ', ' + minutesAway3 + '</div>';
+		var s =  '<div class="trainTimeWrap"><div class="nextTrain">' + minutesAway + ' <span class="thenTrains"> ' + time + '</span></div><div class="thenTrains">' +  minutesAway2 + ', ' + minutesAway3 + ', ' + minutesAway4 +'</div></div>';
 		return s;
 	},
 /*
